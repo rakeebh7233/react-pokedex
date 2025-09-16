c47dba6ef13d35bd58024e31f165699acb2eb4f4
Rakeeb Hossain <hossainrakeeb7233@gmail.com>
2025-09-16 01:01:42 -0400

    PokeCard 50% implemented

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -1,10 +1,19 @@
 import { useState, useEffect } from "react";
-import { getPokedexNumber } from "../utils";
+import { getFullPokedexNumber, getPokedexNumber } from "../utils";
+import TypeCard from "./TypeCard";
 
-export function PokeCard(props) {
+export default function PokeCard(props) {
     const { selectedPokemon } = props;
     const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(false); 
+    const [loading, setLoading] = useState(false);
+
+    const { name, height, abilities, stats, types, moves, sprites } = data || {};
+
+    const imgList = Object.keys(sprites || {}).filter(val => {
+        if (!sprites[val]) return false;
+        if (['versions', 'other'].includes(val)) return false;
+        return true;
+    })
 
     useEffect(() => {
         // if loading, exit logic
@@ -13,14 +22,14 @@ export function PokeCard(props) {
         // check if selectedPokemon is in cache
         // 1. define cache
         let cache = {};
-        if (localStorage.getItem('pokedex')){
+        if (localStorage.getItem('pokedex')) {
             cache = JSON.parse(localStorage.getItem('pokedex'));
         }
         // 2. check if in cache
         if (selectedPokemon in cache) {
             setData(cache[selectedPokemon]);
             return;
-        } 
+        }
         // 3. if not, fetch from API and store in cache
         async function fetchPokemonData() {
             setLoading(true);
@@ -31,19 +40,58 @@ export function PokeCard(props) {
                 setData(pokemonData);
                 console.log(pokemonData);
                 cache[selectedPokemon] = pokemonData;
-                localStorage.setItem('pokedex',JSON.stringify(cache));
+                localStorage.setItem('pokedex', JSON.stringify(cache));
             } catch (error) {
                 console.error("Error fetching Pokémon data:", error);
             } finally {
                 setLoading(false);
             }
         }
-         
+
         fetchPokemonData();
 
     }, [selectedPokemon]);
 
+    if (loading || !data) {
+        return (
+            <div className="poke-card"> Loading...</div>
+        )
+    }
     return (
-        <div className="poke-card"></div>
+        <div className="poke-card">
+            <div>
+                <h4>#{getFullPokedexNumber(selectedPokemon)}</h4>
+                <h2>{name}</h2>
+            </div>
+            <div className="type-container">
+                {types.map((typeObj, typeIndex) => {
+                    return (
+                       <TypeCard key={typeIndex} type={typeObj?.type?.name} /> 
+                    )
+                })}
+            </div>
+            <img className="'default-img" src={'/pokemon/' + 
+                getFullPokedexNumber(selectedPokemon) + '.png'} alt={`${name}-large-img`} />
+            <div className="img-container">
+                {imgList.map((spriteUrl, spriteIndex) => {
+                    const imgUrl = sprites[spriteUrl];
+                    return (
+                        <img key={spriteIndex} src={imgUrl} alt={`${name}-${spriteUrl}-img`} />
+                    )
+                })}
+            </div>
+            <h3>Stats</h3>
+            <div className="stats-card">
+                {stats.map((statObj, statIndex) => {
+                    const {stat, base_stat} = statObj;
+                    return (
+                        <div key={statIndex} className="stat-item">
+                            <p>{stat?.name.replaceAll("-", " ")}</p>
+                            <p>{base_stat}</p>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
     )
 }
--- src/components/TypeCard.jsx
+++ src/components/TypeCard.jsx
@@ -1,5 +1,11 @@
-export function TypeCard() {
+import { pokemonTypeColors } from "../utils";
+
+export default function TypeCard(props) {
+    const { type } = props;
     return (
-        <div className="type-card"></div>
+        <div className="type-tile" style={{ color: pokemonTypeColors?.
+        [type]?.color, background: pokemonTypeColors?.[type]?.background }}>
+            <p>{type}</p>
+        </div>
     )
 }
